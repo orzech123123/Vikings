34ed58d58be0f4747f258953c4272067a891af99
orzech <michalorzechowski123@gmail.com>
2020-02-04 21:10:23 +0100

    Added animated character and ice chunks

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick _joystick;
+    private Animator _animator;
+    private Rigidbody _rigidBody;
+    [SerializeField]
+    private float _jumpForce = 3f;
+
+    private bool _isJumping = false;
+
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        var moveDirection = _joystick.Direction;
+
+        if(moveDirection != Vector2.zero)
+        {
+            transform.position += transform.forward * Time.deltaTime * moveDirection.y * 1.8f;
+            transform.RotateAround(transform.position, transform.up, Time.deltaTime * moveDirection.x * 20f);
+
+            _animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _isJumping = true;
+            _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+
+            _animator.SetBool("isJumping", _isJumping);
+        }
+
+    }
+}
